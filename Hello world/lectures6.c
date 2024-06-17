0db6cc1c3a0f19b2975549057f0804f75ba17fd8
hghg919 <tgeq1023@naver.com>
2024-06-17 22:32:21 +0900

    2024.06.17 연산자 우선순위

--- Hello world/lectures6.c
+++ Hello world/lectures6.c
@@ -3,64 +3,66 @@
 * 작성일   : 2024-06-17
 * 학습목표 : 디버깅
 * 
-* 디버깅
-* - 디버깅을 해야 하는 이유
-* - 프로그래밍을 하면서 작성한 논리적인 문제를 해결하기 위해서
-* - 문제가 의심가는 부분에 코드의 중단점을 생성하고 실제로 텀퓨터의 계산 결과를 눈으로 확인할 수 있다.
-* - 문제를 해결 하기 위한 방법이다(with computer debuging system)
-* 단축키
+*  디버깅
+*  - 디버깅을 해야 하는 이유
+*  - 프로그래밍을 하면서 작성한 논리적인 문제를 해결하기 위해서
+*  - 문제가 의심가는 부분에 코드의 중단점을 생성하고 실제로 컴퓨터의 계산결과를 눈으로 확인할 수 있다.
+*  - 문제를 해결 하기 위한 방법이다(With Computer Debuging System)
+*  단축키
 */
 
-/* operator 연산자
-* 대입, 산술 연산자
-* '=', -, +, /, %
-* '=' : a = b 같다.(논리 연산자 - 수학) (대입 연산자);
-* int num = 30; // left velue = right value, 오른쪽 값을 왼쪽 변수에 넣어주는 기능
-* 결합 순서가 (a <- b) 정해져있다.
-*/
+// Operator 연산자
+// 대입, 산술 연산자
+// '=', -, +, x, / , % 
+// "=" : A = B 같다.(논리 연산자 - 수학)	(대입 연산자);
+// int num = 30;   // Left Value = Right Value; 오른쪽 값을 왼쪽 변수에 넣어주는 기능
+// 결합 순서가 (a <- b ) 정해져있다.
 
 /*복합 대입 연산자
-* 종류 : a = a + b;  a += b;
-*       a = a - b;   a -= b;
-        a = a * b;   a *= b;
-		a = a >> b;  a >>= b;
-		a = a & b;   a &= b;
-		a = a | b;   a |= b;
+* 종류 :  a = a + b;	   a += b;
+*        a = a - b;    a -= b;
+*        a = a * b;    a *= b;
+*        a = a >> b;   a >>= b;
+*        a = a & b;    a &= b;
+*        a = a | b;    a |= b;
 */
 
 /* 증가, 감소 연산자
-* num++;
+*  num++;
 * ++num;
 * num--;
 * --num;
-* ++ : 변수 +=1과 같은 뜻.
-* -- : 변수 -=1과 같은 뜻.
-* 전위 증가(감소) : 변수에 1을 먼저 더 해준 후, 문장의 연산을 진행한다.
-* 후위 증가(감소) : 문장의 연산을 먼저 진행 후 변수의 값을 변화 시킨다.
+*  ++ : 변수 +=1와 같은 뜻.
+*  -- : 변수 -=1와 같은 뜻.
+*
+*  전위 증가(감소) : 변수에 1을 먼저 더 해준 후, 문장의 연산을 진행한다.
+*  후위 증가(감소) : 문장의 연산을 먼저 진행 후 변수의 값을 변화 시킨다.
+*
 */
+
 
 
 /* 관계 연산자
-* 비교 연산자 : 두개의 값을 비교하여 그 관계를 표현하는 연산자.
-* a, b;
-* a = 10, b = 12;
-* 결과 값이 ture(참), false(거짓)으로 나옵니다.
-* ture = 1, false = 0
-* result = (a>b);
-* result의 값이 ture면 1, false면 0
+*  비교 연산자. 두 개의 값을 비교하여 그 관계를 표현하는 연산자.
+*  a ,  b , result ;
+*  a = 10, b = 12
+*  결과 값이 True(참), False(거짓)으로 나옵니다.
+*  True : 1,  False : 0
+*  result = (a > b);
+*  result의 값이 true면 1이, false면 0이 들어간다.
 */
 
 /* 논리 연산자
-* and, or, not
-* and : && (shift + 7) -> a와 b가 모두 참이면 결과를 참인 결과를 반환한다. "1"
-* or : || (shift + \(달러)) -> a와 b중 하나라도 참이면 1인 결과를 반환한다.
-* not : !변수 -> a가 true이면 false 반환, false이면 true를 반환한다.
+*  AND, OR, NOT
+*  AND : && (shift + 7)	       ->  A와 B가 모두 참이면 결과를 참인 결과를 반환한다. "1"
+*  OR  : || (Shift + \(달러))  ->  A와 B중 하나라도 참이면 1인 결과를 반환한다.
+*  NOT : !변수 	               -> A가 True이면 False 반환, False이면 True 반환한다.
 */
 
 // 연산 결합 순위
-// int a = (a ==10 && b == 12);
-// int b = (a < 12 || b > 12);
-// int c = !a;
+// int A = (a == 10 && b == 12);
+// int B = (a < 12 || b > 12);
+// int C = !a; 
 
 #include "lectures.h"
 
@@ -75,7 +77,7 @@ void lectures6()
 
 	printf("연산자 예제 문제\n\n");
 
-	// 9 + 2 = 11, 9 - 2 = 7; 로컬 호출스택
+	// 9 + 2 = 11, 9 - 2 = 7; 자동 호출스택
 
 	int num1, num2, num3, result;
 	result = 0;
@@ -98,16 +100,16 @@ void lectures6()
 
 	int variablea; // 3 + 4 X 5;
 	int variableb; // 7 / 3 % 2;
-	int final = variablea < variableb; // variablea와 variableb를 관계연산자를 사용하여 final 대입해보세요.
+	int final; // variablea와 variableb를 관계연산자를 사용하여 final 대입해보세요.
 
 	// scanf 작업 데이터의 할당
 
 	int cal1, cal2, cal3;
-	scanf_s("%d %d %d \n", &cal1, &cal2, &cal3);
+	scanf_s("%d %d %d\n", &cal1, &cal2, &cal3);
 	variablea = cal1 + cal2 * cal3;
-	printf("variablea의 값 : %d", variablea);
+	printf("variablea의 값 : %d \n", variablea);
 	variableb = 7 / 3 % 2;
+	final = (variablea < variableb);
 
-	printf("result : %d \n", result <= final);
-	//집가서 확인
+	printf("variablea가 variableb 보다 작나요? ture이면 1을 출력 false이면 0을 출력합니다 : %d\n", final);
 }
