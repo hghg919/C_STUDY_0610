f53660f079ce544562cf4b4f33fa479b6fe04a5e
hghg919 <tgeq1023@naver.com>
2024-06-26 22:48:54 +0900

    수정

--- MYGame/main.c
+++ MYGame/main.c
@@ -24,18 +24,51 @@ int main(void)
 	printf("| ☆★☆★☆★☆★☆★☆★☆★☆ |\n");
 	printf("| ☆★☆★☆★☆★☆★☆★☆★☆ |\n");
 	printf("| ☆★☆★☆★☆★☆★☆★☆★☆ |\n");
-	printf("| ☆★★☆★게임 시작!★☆★★☆ |\n");
+	printf("| ☆★★☆★숫자 게임!★☆★★☆ |\n");
 	printf("| ☆★☆★☆★☆★☆★☆★☆★☆ |\n");
 	printf("| ☆★☆★☆★☆★☆★☆★☆★☆ |\n");
 	printf("| ☆★☆★☆★☆★☆★☆★☆★☆ |\n\n");
 
-	printf("숫자를 입력하면 게임이 시작합니다.\n");  // 정해진 숫자만 입력하게 <0 : 게임종료> <1 : 게임시작> 그외 숫자는 숫자를 다시 입력하게
-	int start_num;
-	scanf_s("%d", &start_num);
+	printf("1을 입력하면 게임을 시작합니다.\n");  // 정해진 숫자만 입력하게 <0 : 게임종료> <1 : 게임시작> 그외 숫자는 숫자를 다시 입력하게
+	printf("0을 입력하면 게임을 종료합니다.\n");
+
+	while (1)
+	{
+		int start_num;
+		scanf_s("%d", &start_num);
+
+		if (start_num == 0) // 종료가 안됨 수정이 필요
+		{
+			printf("게임을 종료합니다.\n\n");
+			break;
+		}
+		else if(start_num == 1)
+		{
+			printf("게임 시작!\n\n");
+			break;
+		}
+		else
+		{
+			printf("잘못된 입력입니다. 0 또는 1을 입력해주세요.\n");
+		}
+	}
 
 	printf("1 ~ 100 범위의 플레이어의 값을 넣어주세요.\n"); // 1 ~ 100 이외의 숫자는 다시 입력하게
-	scanf_s("%d", &uservalue);
+	
+	while (1)
+	{
+		scanf_s("%d", &uservalue);
 
+		if (uservalue < 1 || 100 < uservalue)
+		{
+			printf("잘못된 값입니다.\n");
+			printf("1 ~ 100 범위의 플레이어의 값을 넣어주세요.\n\n");
+		}
+		else
+		{
+			break;
+		}
+	}
 	printf("\n");
 
 	//printf("컴퓨터의 값은 %d 입니다.\n", computervalue);
@@ -53,7 +86,7 @@ int main(void)
 
 		if (uservalue == computervalue)
 		{
-			printf("축하합니다! 게임을 클리어하셨습니다. \n컴퓨터의 값\n : %d", computervalue);
+			printf("축하합니다! 게임을 클리어하셨습니다. \n컴퓨터의 값 : %d", computervalue);
 			break;
 		}
 		else
@@ -79,6 +112,7 @@ int main(void)
 			}
 			if (ab == 1)
 			{
+				printf("게임종료 ^모^\n");
 				printf("컴퓨터의 값은 %d 입니다.\n", computervalue);
 			}
 			
