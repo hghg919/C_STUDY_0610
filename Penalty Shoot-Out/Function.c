eb4a467f0e712e1c51e3134b4a13e5ead11b6280
hghg919 <tgeq1023@naver.com>
2024-07-11 16:03:56 +0900

    게임 제작

--- Penalty Shoot-Out/Function.c
+++ Penalty Shoot-Out/Function.c
@@ -5,31 +5,69 @@
 
 void GameStart(void)
 {
-	while (1) // 정해진 숫자만 입력하게 <0 : 게임종료> <1 : 게임시작> 그외 숫자는 숫자를 다시 입력하게
-	{
-		printf("승부차기 게임\n\n");
-		printf("게임시작(1)\n"); // 수를 입력하는 것이 아닌 게임시작과 게임종료를 선택해서 고를 수는 없을까
-		printf("게임종료(0)\n");
-		int start_num;
-		scanf_s("%d", &start_num);
-		Clear();
+		printf("\n \n \n \n");
+		printf("    ######   ######   #    #     ##     #         #####    #   #          #####   #    #     ##       ##      #####              ###    #    #    #####\n");
+	    printf("    ##   ##  #        ##   #    #  #    #           #      #   #         ##   ##  #    #    #  #     #  #       #               ## ##   #    #      #\n");
+	    printf("    ##   ##  #        # #  #   #    #   #           #      #   #         ##       #    #   #    #   #    #      #              ##   ##  #    #      #\n");
+		printf("    ######   ####     #  # #   ######   #           #       ###           #####   ######   #    #   #    #      #     ## ###   ##   ##  #    #      #\n");
+		printf("    ##       #        #   ##   #    #   #           #        #                ##  #    #   #    #   #    #      #      ### ##  ##   ##  #    #      #\n");
+		printf("    ##       #        #    #   #    #   #           #        #           ##   ##  #    #    #  #     #  #       #               ## ##   #    #      #\n");
+		printf("    ##       ######   #    #   #    #   ######      #        #            #####   #    #     ##       ##        #                ###     ####       #\n");
+		
+		int x = 67;
+		int y = 18;
 
-		if (start_num == 0)
-		{
-			printf("게임을 종료합니다.");
-			exit(0); //return; : 함수의 실행을 종료시킴 값반환x // return 0; : 정수 값을 반환시킴, 함수종료 // exit(0); : 프로그램을 종료시킴
-		}
-		else if (start_num == 1)
-		{
-			break;
-		}
-		else
+		GotoXY(x,y);
+		printf("  게 임 시 작 \n"); // 게임시작과 게임종료를 키로 선택하여 실행
+		GotoXY(x,y+1);
+		printf("  게 임 종 료 \n");
+		GotoXY(x-2, y);
+		printf(">");
+
+		while (1)
 		{
 
+			if (GetAsyncKeyState(VK_UP) & 8001) // 
+			{
+				if (y > 18)
+				{
+					GotoXY(x - 2, y);
+					printf(" ");
+					GotoXY(x - 2, --y);
+					printf(">");
+				}
+			}
+			else if (GetAsyncKeyState(VK_DOWN) & 8001)
+			{
+				if (y < 19)
+				{
+					GotoXY(x - 2, y);
+					printf(" ");
+					GotoXY(x - 2, ++y);
+					printf(">");
+				}
+			}
+			else if (GetAsyncKeyState(VK_SPACE) & 8001) // 선택키 // 게임시작 선택시 게임화면으로 // 게임종료 선택시 종료후 콘솔창 끄기
+			{
+				if (y == 18)
+				{
+					Clear();
+					printf("게임 화면입니다.\n 개발중~");
+				}
+				if (y == 19)
+				{
+					exit(0);
+				}
+			}
+			// else if (GetAsyncKeyState(VK_ESCAPE) & 8001) // 종료키
+			// {
+			// 	exit(0);
+			// 	FreeConsole();
+			// }
+		    Sleep(100);
 		}
-	}
 }
-
+	
 void Clear(void)
 {
 	system("cls");
@@ -45,4 +83,16 @@ void SetConsoleSize(int _col, int _lines)
 	char setText[100];
 	sprintf_s(setText, 100, "mode con cols=%d lines=%d", _col, _lines);
 	system(setText);
+}
+
+void GotoXY(int _x, int _y)
+{
+	COORD pos = { _x, _y };
+
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+void GameMain(void)
+{
+
 }
